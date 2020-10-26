3687045d346a3b044fce6fea4651487dd260514d
Milo123459 <milo5huntington@gmail.com>
2020-10-26 17:17:09 +0000

    add typings

---
+++ src/typings/index.d.ts
@@ -0,0 +1,8 @@
+declare function def(str: string): string;
+declare function array(str: string[]): string[];
+declare function keep(str: string): string;
+declare function all(str: string): string;
+declare function sent(str: string): string;
+declare function dash(str: string): string;
+declare function reg(str: string): string;
+export { def, array, keep, all, sent, dash, reg };
