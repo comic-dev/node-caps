9b3d2f1ffed0bd2238a1ab1cda62bdd2c70d7748
comic. dev <simplyanimatedvideos@gmai.com>
2021-03-18 15:54:51 +0100

    fix(import): Fix import issue

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import * as Func from "./func/index";
-export default {
-  ...Func,
-  version: require("../package.json").version,
-};
--- src/typings/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare function def(str: string): string;
-declare function array(str: string[]): string[];
-declare function keep(str: string): string;
-declare function all(str: string): string;
-declare function sent(str: string): string;
-declare function dash(str: string): string;
-declare function reg(str: string): string;
-declare function perms(str: string[]): string[];
-export { def, array, keep, all, sent, dash, reg, perms };
