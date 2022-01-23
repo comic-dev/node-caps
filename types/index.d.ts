abfa46085e5c2c7f9e4574f273cab8e4ec4632c5
comic-dev <66639833+comic-dev@users.noreply.github.com>
2022-01-23 11:38:20 +0100

    Fix types and add declaration files

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,4 +5,4 @@ export function array(arr: string[]): string;
 export function sent(str: string): string;
 export function dash(str: string): string; 
 export function reg(str: string): string;
-export function perms(str: string): string;
+export function perms(str: string[]): string[];
