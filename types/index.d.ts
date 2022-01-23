db4c51e6720243af6dabe32df57daeaa606f0ddb
comic <66639833+comic-dev@users.noreply.github.com>
2022-01-23 11:14:16 +0100

    Create index.d.ts

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+export function def(str: string): string;
+export function keep(str: string): string;
+export function all(str: string): string;  
+export function array(arr: string[]): string; 
+export function sent(str: string): string;
+export function dash(str: string): string; 
+export function reg(str: string): string;
+export function perms(str: string): string;
